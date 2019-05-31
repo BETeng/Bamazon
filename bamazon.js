93c0a1c320e124a0528d660798746a2eebd190b1
BETeng <brian7.teng@gmail.com>
2019-05-31 01:49:45 -0500

    added initial startup function and sudocode

--- bamazon.js
+++ bamazon.js
@@ -3,35 +3,60 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: 'localhost',
-  user:'root',
-  password:'quincy99',
-  database:'bamazonDB'
+  user: 'root',
+  password: 'quincy99',
+  database: 'bamazonDB'
 });
 
-connection.connect(function(err){
-  if(err) throw err;
-  console.log('connected as id '+connection.threadId);
+connection.connect(function (err) {
+  if (err) throw err;
+  showItems();
   toBuy();
-
-  
 })
 
-function toBuy(){
+function showItems() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    console.table(res);
+  })
+}
+
+function toBuy() {
   inquirer
     .prompt({
-      name:'item',
-      type:'input',
+      name: 'item',
+      type: 'input',
       message: 'Enter the ID number of the item you would like to buy.'
     })
-    .then(function(answer){
+    .then(function (answer) {
       var query = "SELECT * FROM products WHERE item_id=?";
-      connection.query(query,[(answer.item)], function(err, res){
+      connection.query(query, [(answer.item)], function (err, res) {
         console.table(res)
-        
-        
       })
-        
-        }
-        )
     }
+    )
+}
 
+function howMany(){
+  inquirer
+  .prompt({
+    name: 'quantity',
+    type: 'input',
+    message: 'How many of the following item would you like to buy?'
+  })
+  .then(function(answer){
+    //compare amount of item to stock_quantity
+      // if (stock_quantity >= answer.quantity){
+      //   allow transaction to go thru
+      //   log price * answer.quantity
+      //   subtract answer.quantity from stock_quantity
+      //   redisplay the table}
+      // else{
+      //   console.log("Insufficient Quantity!");
+      //   toBuy();
+      // }
+
+
+
+
+      })
+    } //howmany function closing bracket
