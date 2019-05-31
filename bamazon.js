abd154276f480f1bc24250dfe200baa95ac0ba41
BETeng <brian7.teng@gmail.com>
2019-05-31 01:15:25 -0500

    toBuy function working

--- bamazon.js
+++ bamazon.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -10,5 +11,27 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
   if(err) throw err;
   console.log('connected as id '+connection.threadId);
-  connection.end();
+  toBuy();
+
+  
 })
+
+function toBuy(){
+  inquirer
+    .prompt({
+      name:'item',
+      type:'input',
+      message: 'Enter the ID number of the item you would like to buy.'
+    })
+    .then(function(answer){
+      var query = "SELECT * FROM products WHERE item_id=?";
+      connection.query(query,[(answer.item)], function(err, res){
+        console.table(res)
+        
+        
+      })
+        
+        }
+        )
+    }
+
